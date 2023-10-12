6f7b9adb87ffc764b59f7e0a536bc4f08bb1b80c
Marcos Lara <codmoderwarfare@gmail.com>
2023-10-12 14:59:35 -0300

    Removed the testing bit

--- pages/App/Main.tsx
+++ pages/App/Main.tsx
@@ -371,10 +371,6 @@ export default function Main() {
                       className="w-5 h-5 ml-2 cursor-pointer"
                       onClick={() => {
                         enableNotificatinons();
-                        handleNotificatinons({
-                          headerSubject: 'Teste',
-                          text: 'testeee',
-                        });
                       }}
                     >
                       <path d="M5.85 3.5a.75.75 0 00-1.117-1 9.719 9.719 0 00-2.348 4.876.75.75 0 001.479.248A8.219 8.219 0 015.85 3.5zM19.267 2.5a.75.75 0 10-1.118 1 8.22 8.22 0 011.987 4.124.75.75 0 001.48-.248A9.72 9.72 0 0019.266 2.5z" />
